e3d45d3569a516c4b73031ba335ee11d17bbee5a
gersue <GersonCruz301115@Gmail.com>
2022-05-27 10:23:56 -0600

    Metodo post utilizando manejo de excepciones

--- VetAdmin/Controllers/ClientesController.cs
+++ VetAdmin/Controllers/ClientesController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using VetAdmin.Context;
 using System.Linq;
 using VetAdmin.Models;  //Se importa para el Response del metodo GET
+using System;
 
 namespace VetAdmin.Controllers
 {
@@ -21,13 +22,24 @@ namespace VetAdmin.Controllers
         }
 
         [HttpPost]
-        public Response POST(Cliente cliente)
+        public Response Post(Cliente cliente)
         {
-            db.Clientes.Add(cliente);
+            if(cliente.Noidentificacion == null)
+            {
+                return new Response { Success = false, Message = "No_Identificacion es un dato requerido" };
+            }
 
-            db.SaveChanges();
+            try {
+                db.Clientes.Add(cliente);
 
-            return new Response { Success = true, Data = cliente };
+                db.SaveChanges();
+
+                return new Response { Success = true, Data = cliente };
+            }
+            catch(System.Exception ex) {
+                Console.WriteLine(DateTime.Now + " - " +ex.Message + "Inner Details: "+ ex.InnerException.Message);
+                return new Response { Success = false, Message = "Se ha producido un error" };
+            }
         }
     }
 }
